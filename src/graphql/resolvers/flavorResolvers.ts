383ab7a75dd3fb8cca8959f66df593663468a045
Andrew M. Pankratz <apankra@wgu.edu>
2025-08-26 13:30:49 -0400

    Connect create, update, and delte flavor mutation resolvers to db

--- src/graphql/resolvers/flavorResolvers.ts
+++ src/graphql/resolvers/flavorResolvers.ts
@@ -1,13 +1,15 @@
+import { createFlavor, deleteFlavor, getFlavor, getFlavors, updateFlavor } from '../../db/pairings/crud-flavor';
 import { Resolvers } from '../../generated/graphql';
 
 export const resolvers: Resolvers = {
 	Query: {
-		flavor: (_root, { name }) => {
-			return { name: name };
-		},
-		flavors: () => {
-			return [{ name: 'Vanilla' }, { name: 'Chocolate' }];
-		},
+		flavor: (_root, { name }) => getFlavor(name),
+		flavors: () => getFlavors(),
+	},
+	Mutation: {
+		createFlavor: (_root, { input: { name } }) => createFlavor({ name }),
+		updateFlavor: (_root, { input: { name, updatedName } }) => updateFlavor({ name, updatedName }),
+		deleteFlavor: (_root, { name }) => deleteFlavor(name),
 	},
 	Flavor: {
 		pairings: flavor => {
