4695ca4d28be3aa4ac11f1b107cda279cd08fdbb
Andrew M. Pankratz <apankra@wgu.edu>
2025-08-28 12:12:49 -0400

    Add express security measures

--- src/server/server.ts
+++ src/server/server.ts
@@ -11,10 +11,27 @@ import { resolvers as volumeResolvers } from '../graphql/resolvers/volumeResolve
 import { resolvers as weightResolvers } from '../graphql/resolvers/weightResolvers';
 import { resolvers as techniqueResolvers } from '../graphql/resolvers/techniqueResolvers';
 import Config from '../config/config';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+import authNeo4j from '@ampkz/auth-neo4j';
+import { User } from '@ampkz/auth-neo4j/user';
+import { validateSessionToken } from '@ampkz/auth-neo4j/token';
+import cors from 'cors';
+import { Auth } from '@ampkz/auth-neo4j/auth';
 
-export interface MyContext {}
+export interface MyContext {
+	authorizedUser?: User;
+}
 
 async function startServer() {
+	const limiter = rateLimit({
+		windowMs: 15 * 60 * 1000, // 15 minutes
+		max: 100, // Limit each IP to 100 requests per windowMs
+		standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+		legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+		message: 'Too many requests from this IP, please try again later.',
+	});
+
 	const app = express();
 	const httpServer = http.createServer(app);
 
@@ -31,16 +48,57 @@ async function startServer() {
 		typeDefs,
 		resolvers: [flavorResolvers, tasteResolvers, volumeResolvers, weightResolvers, techniqueResolvers],
 		includeStacktraceInErrorResponses: Config.IS_NOT_PROD,
+		introspection: Config.IS_NOT_PROD,
 		plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 	});
 
 	await server.start();
 
+	/* istanbul ignore next line */
+	const helmetOptions = Config.IS_NOT_PROD
+		? {
+				crossOriginEmbedderPolicy: false,
+				contentSecurityPolicy: {
+					directives: {
+						imgSrc: [`'self'`, 'data:', 'apollo-server-landing-page.cdn.apollographql.com'],
+						scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+						manifestSrc: [`'self'`, 'apollo-server-landing-page.cdn.apollographql.com'],
+						frameSrc: [`'self'`, 'sandbox.embed.apollographql.com'],
+					},
+				},
+		  }
+		: {};
+
+	app.use(helmet(helmetOptions));
+
+	/* istanbul ignore next line */
+	app.use(
+		cors<cors.CorsRequest>({
+			origin: `${Config.IS_NOT_PROD ? [`http://localhost:3000`, 'https://studio.apollographql.com'] : ``}`,
+			methods: 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+			credentials: true,
+		})
+	);
+
 	app.use(express.json());
 	app.use(express.urlencoded({ extended: true }));
+	app.use(limiter);
+	/* istanbul ignore next line */
+	app.use(
+		'/graphql',
+		expressMiddleware(server, {
+			context: async ({ req }) => {
+				if (Config.IS_NOT_PROD) {
+					return { authorizedUser: new User({ email: 'apollo', auth: Auth.CONTRIBUTOR }) };
+				}
 
-	app.use('/graphql', expressMiddleware(server));
-
+				const token = req.cookies.token;
+				const svr = await validateSessionToken(token);
+				return { authorizedUser: svr.user };
+			},
+		})
+	);
+	app.use(authNeo4j());
 	return app;
 }
 
